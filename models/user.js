d72a1ffc91d4a5bb4335de83819b2cf3ef95dc38
Veronica Ni <vni1294@gmail.com>
2024-01-08 15:03:14 -0500

    Implemented models/user.js User authenticate

--- models/user.js
+++ models/user.js
@@ -36,6 +36,20 @@ class User {
   /** Authenticate: is username/password valid? Returns boolean. */
 
   static async authenticate(username, password) {
+    const result = await db.query(
+      `SELECT password
+          FROM users
+          WHERE username = $1`, 
+        [username]);
+
+    const user = result.rows[0];
+
+    if (user) {
+      if (await bcrypt.compare(password, user.password) === true) {
+        return true;
+      }
+    }
+    return false;
   }
 
   /** Update last_login_at for user */
