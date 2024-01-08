0351ab458ea21470064985a51049457474a0db69
Veronica Ni <vni1294@gmail.com>
2024-01-08 18:13:13 -0500

    Updated seed user message SQL

--- messagely.sql
+++ messagely.sql
@@ -23,6 +23,25 @@ CREATE TABLE messages (
   sent_at TIMESTAMP WITH TIME ZONE NOT NULL,
   read_at TIMESTAMP WITH TIME ZONE);
 
+INSERT INTO users ( 
+  username,
+  password,
+  first_name,
+  last_name,
+  phone,
+  join_at)
+VALUES ('johnsmith', 'password', 'John', 'Smith', '333.333.3333', '2020-01-01 12:00'),
+        ('janedoe', 'password', 'Jane', 'Doe', '333.333.3333', '2020-01-01 12:00');
+
+INSERT INTO messages ( 
+  from_username,
+  to_username,
+  body,
+  sent_at,
+  read_at)
+VALUES ('janedoe', 'johnsmith', 'Hi John!', '2023-12-25 12:30', '2024-1-25 12:30'),
+('johnsmith', 'janedoe', 'sup', '2024-1-25 12:30', '2024-1-25 12:35');
+
 
 \echo 'Delete and recreate messagely_test db?'
 \prompt 'Return for yes or control-C to cancel > ' foo
@@ -47,25 +66,6 @@ CREATE TABLE messages (
   body TEXT NOT NULL,
   sent_at TIMESTAMP WITH TIME ZONE NOT NULL,
   read_at TIMESTAMP WITH TIME ZONE);
-
-INSERT INTO users ( 
-  username,
-  password,
-  first_name,
-  last_name,
-  phone,
-  join_at)
-VALUES ('johnsmith', 'password', 'John', 'Smith', '333.333.3333', '2020-01-01 12:00'),
-        ('janedoe', 'password', 'Jane', 'Doe', '333.333.3333', '2020-01-01 12:00');
-
-INSERT INTO messages ( 
-  from_username,
-  to_username,
-  body,
-  sent_at,
-  read_at)
-VALUES ('janedoe', 'johnsmith', 'Hi John!', '2023-12-25 12:30', '2024-1-25 12:30'),
-('johnsmith', 'janedoe', 'sup', '2024-1-25 12:30', '2024-1-25 12:35');
 
 
 
