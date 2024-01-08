11f3f662684920c6856acb434ea35a76142a7456
Veronica Ni <vni1294@gmail.com>
2024-01-08 15:12:22 -0500

    Implemented models/user.js User get user

--- models/user.js
+++ models/user.js
@@ -83,6 +83,13 @@ class User {
    *          last_login_at } */
 
   static async get(username) {
+    const results = await db.query(
+      `SELECT username, first_name, last_name, phone, join_at, last_login_at
+          FROM users
+          WHERE username = $1`
+        [username]);
+
+    return results.rows[0];
   }
 
   /** Return messages from this user.
